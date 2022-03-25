a065e23edefb5ee6d0c5ff2ba31cbe14c9312cef
Thawany22 <souzaathaay4@gmail.com>
2022-03-25 19:47:00 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(@"Eu nem sei o que dizer quando falo de você
+Eu não sei me expressar, eu nem sei o que fazer
+De todas as vidas que eu tenho pra viver
+Eu quero ficar do seu lado até o final
+Toma decisões ruins faz parte de nóis
+O mar do coração se acalma quando ouço sua voz
+Será que te vejo a sós?
+Me dá uma chance de tirar seu gloss
+Eu amo quando pede pra eu escolher suas coisas
+Eu acho tão lindo seu piercing ao lado da boca, aham
+Espero que vire minha noiva
+Ou até mesmo que vire outra coisa
+Você é de longe e eu daqui
+Posso te levar um açaí, ya
+Linda pra casa você não volta mais
+Quero que aqui seja seu lugar de paz
+Jura?
+Porque cê mexe comigo?
+Jura que eu sempre vou ser seu menino?
+Tá me iludindo com esse seu sorriso
+Eu sei que cê vai achar estranho tudo isso
+Ei amor, amor, ah, ei amor, amor, ah
+Quero te ver sem nada de amor cê não vai ter falta
+Ei amor, amor, ah, ei amor, amor, ah
+Quero te ver sem nada de amor cê não vai ter falta
+Nova moda, nova moda
+Nova moda, nova moda");
